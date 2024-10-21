dd2487abf342e53e3aa81fb41ba2d85f4c8b1d17
Santiago Rosado Raya <sanrosray@alum.us.es>
2024-10-21 19:23:46 +0200

    Practica 1 done

--- app/modules/notepad/repositories.py
+++ app/modules/notepad/repositories.py
@@ -5,3 +5,6 @@ from core.repositories.BaseRepository import BaseRepository
 class NotepadRepository(BaseRepository):
     def __init__(self):
         super().__init__(Notepad)
+
+    def get_all_by_user(self, user_id):
+        return Notepad.query.filter_by(user_id=user_id).all()
